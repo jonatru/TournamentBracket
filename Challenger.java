29ec14855a4b9a077b48ed9f72bbd8f09dfda32c
bjonen1 <jonenb@gmail.com>
2018-05-03 20:14:09 -0500

    Commented challenger.java

--- Challenger.java
+++ Challenger.java
@@ -1,23 +1,35 @@
 package application;
-
+/*
+ * 
+ */
 public class Challenger {
-	private String name;
-	private Integer rank;
+	private String name;//Name of the challenger
+	private Integer rank;//rank of the challenger.
 
+	/*Constructor. Initializes team name to TBD and rank to -1.
+	 * 
+	 */
 	public Challenger() {
 		name = "TBD";
 		rank = -1;
 	}
 
+	/*Additional Contructor initializing name and rank to those given.
+	 * 
+	 */
 	public Challenger(String name, Integer rank) {
 		this.name = name;
 		this.rank = rank;
 	}
-	
+	/*
+	 * Returns the name of the Challenger
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/*
+	 * Sets the name of the challenger. returns false if attempting to use a null name
+	 */
 	public boolean setName(String name) {
 		if (name == null) {
 			return false;
@@ -26,10 +38,16 @@ public class Challenger {
 		return true;
 	}
 	
+	/*
+	 * Returns rank of the challenger
+	 */
 	public Integer getRank() {
 		return this.rank;
 	}
 	
+	/*
+	 * Sets the rank of the challenger.
+	 */
 	public Boolean setRank(Integer rank) {
 		if (rank == null) {
 			return false;
