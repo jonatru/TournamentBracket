d233663164a8019b58b425f7e979a0b3226e3ed7
bjonen1 <jonenb@gmail.com>
2018-05-03 20:42:50 -0500

    Fully commented challenge.java

--- Challenge.java
+++ Challenge.java
@@ -1,5 +1,14 @@
 package application;
-
+///////////////////////////////////////////////////////////////////////////////
+//
+//Title:            JavaFX Tournament Bracket A-team 66
+//Files:            Main.java, Tournament.java, Challenge.java, Challenger.java
+//Semester:         CS 400 Spring 2018
+//
+//Author:           Brandon Jonen,  JOSHUA MATHEWS, MICHAEL O'CONNOR, JONATHON TRUTTMANN
+//Email:            bjonen@wisc.edu
+//CS Login:         jonen,
+//Lecturer's Name:  Deb Deppeler
 /*
  * Challenge represents a match between two challengers in the tournament.
  * Each challenge consists of 2 challengers, 2 scores, a winner, and a complete boolean.
@@ -11,7 +20,7 @@ public class Challenge {
 	private Challenger winner; //the winner of the challenge
 	private Boolean complete;//status as to whether it is complete
 
-	/*
+	/**
 	 * Constructor. creates the challenger array and scores.
 	 */
 	public Challenge() {
@@ -20,15 +29,19 @@ public class Challenge {
 		complete = false;
 	}
 
-	/*
+	/**
 	 * returns the challenger at the index
+	 * @param index
+	 * @return Challenger
 	 */
 	public Challenger getChallenger(int index) {
 		return challengers[index];
 	}
 
-	/*
+	/**
 	 * sets a challenger in the challenge at the index. returns false if given null challenger
+	 * @param index, challenger
+	 * @return Boolean
 	 */
 	public Boolean setChallenger(int index, Challenger challenger) {
 		if (challenger == null) {
@@ -38,40 +51,56 @@ public class Challenge {
 		return true;
 	}
 
-	/*
+	/**
 	 * returns winner of the challenge
+	 * @return Challenger
 	 */
 	public Challenger getWinner() {
 		return winner;
 	}
 
-	/*
+	/**
 	 * sets the winner of the challenge
+	 * @param index
+	 * @return boolean
 	 */
 	public boolean setWinner(int index) {
 		winner = challengers[index];
 		return true;
 	}
 
-	/*
+	/**
 	 * gets the score of the challenger at the index
+	 * @param index
+	 * @return int
 	 */
 	public int getScore(int index) {
 		return scores[index];
 	}
 
-	/*
+	/**
 	 * sets the score at the index
+	 * @param index,score
+	 * @return boolean
 	 */
 	public boolean setScore(int index, int score) {
 		scores[index] = score;
 		return true;
 	}
-
+	
+	/**
+	 * 
+	 * @return boolean
+	 */
 	public boolean getComplete() {
 		return complete;
 	}
 
+	/**
+	 * 
+	 * @param complete
+	 * @return
+	 */
 	public boolean setComplete(boolean complete) {
 		this.complete = complete;
 		return true;
